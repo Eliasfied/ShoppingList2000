9321c011622e67acebd5c186275a7195a2a6bdfe
Elias Fiedler <e.fiedler@eckelmann.de>
2024-03-19 16:23:07 +0100

    cached config files deleted

--- ShoppingList2000Frontend/src/config/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDgmZiZkcuEb5eRlP9iwiGt7ZU4vtXINpc",
-    authDomain: "shoppinglist2000-ed7f9.firebaseapp.com",
-    projectId: "shoppinglist2000-ed7f9",
-    storageBucket: "shoppinglist2000-ed7f9.appspot.com",
-    messagingSenderId: "951031361373",
-    appId: "1:951031361373:web:d72fa86362b63781a442b4"
-};
